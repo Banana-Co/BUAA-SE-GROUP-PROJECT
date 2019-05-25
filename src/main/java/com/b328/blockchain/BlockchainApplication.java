5f32eff6e19c01e7ccf3bfa1d313a1cac619b374
lyx <383625226@qq.com>
2019-05-25 14:30:11 +0800

    2019.5.25

---
+++ src/main/java/com/b328/blockchain/BlockchainApplication.java
@@ -0,0 +1,11 @@
+package com.b328.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainApplication.class, args);
+    }
+}
