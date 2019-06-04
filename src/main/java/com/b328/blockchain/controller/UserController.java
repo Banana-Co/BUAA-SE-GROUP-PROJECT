bbf28f1e2df182c202a7fa1b4ff4bd5e7b6d21df
whosl <982267311@qq.com>
2019-06-04 10:41:46 +0800

    2019.6.4

--- src/main/java/com/b328/blockchain/controller/UserController.java
+++ src/main/java/com/b328/blockchain/controller/UserController.java
@@ -1,19 +1,47 @@
 package com.b328.blockchain.controller;
 
 import com.b328.blockchain.entity.User;
+import com.b328.blockchain.mapper.RoleMapper;
 import com.b328.blockchain.serviceimpl.UserService;
 import com.b328.blockchain.util.UserRegisteAndLogin;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
     @Autowired
     @Qualifier("userService")
     private UserService service;
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    //跳转到登录表单页面
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "need login";
+    }
+
+    //登录成功后，跳转的页面
+    @RequestMapping("/success")
+    public String index(Model model) {
+        return "success";
+    }
+
+    //未登录，可以访问的页面
+    @RequestMapping("/index")
+    public String list(Model model) {
+        return "index";
+    }
+
 
     /**
      * 处理用户的登录请求
@@ -44,5 +72,32 @@ public class UserController {
         service.userRegister(user);
 
         return UserRegisteAndLogin.userLogin(user, model); //使用户沆注册后立马登录
+    }
+    //登出
+    @RequestMapping(value = "/logout")
+    public String logout(){
+        return "logout";
+    }
+
+    //错误页面展示
+    @GetMapping("/403")
+    public String error(){
+        return "error ok!";
+    }
+
+    //管理员功能
+    @RequiresRoles("admin")
+    @RequiresPermissions("管理员添加")
+    @RequestMapping(value = "/admin/add")
+    public String create(){
+        return "add success!";
+    }
+
+    //用户功能
+    @RequiresRoles("user")
+    @RequiresPermissions("用户查询")
+    @RequestMapping(value = "/user/select")
+    public String detail(){
+        return "select success";
     }
 }
